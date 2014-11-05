bb035183b899ea5549ca8daa5c02298e66b3db86
Philippe O. Wagner <wagner@arteria.ch>
2014-11-05 15:13:50 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="django-contact-plus",
+    version="0.0.1",
+    author_email="admin@arteria.ch",
+    packages=find_packages(),
+    include_package_data=True,
+ 
+
+)
